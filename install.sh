26e0512b795a5d300ec906273d8c92d74a1afd69
ibrahim ethem demirci <dmrc_wr@hotmail.com>
2025-06-16 17:39:36 +0300

    Fix install script version handling

--- install.sh
+++ install.sh
@@ -17,7 +17,9 @@ esac
 
 LATEST=$(curl -fsSL "https://api.github.com/repos/$REPO/releases/latest" | \
   grep tag_name | head -n1 | cut -d '"' -f4)
-TARBALL="keptler_${LATEST}_${OS}_${ARCH}.tar.gz"
+# Release assets are named without the leading 'v' prefix used in tag names.
+VERSION="${LATEST#v}"
+TARBALL="keptler_${VERSION}_${OS}_${ARCH}.tar.gz"
 URL="https://github.com/$REPO/releases/download/${LATEST}/${TARBALL}"
 
 tmpdir=$(mktemp -d)
